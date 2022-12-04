fb225aa0f84e46030d675bdb463b955de385210f
Hasibullah Aman <hasib.aman7@gmail.com>
2022-12-04 06:36:46 +0430

    01

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 document.querySelector('h1').innerHTML = "Hasibullah Aman";
+document.getElementsByTagName("li")[2].style.color = "red";
+document.getElementsByClassName("btn").style.color = 'red';
+document.querySelector('#list a').style.color = 'red';
