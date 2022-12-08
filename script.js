68cb853d31950eee625f1cc6884d3b1a023d6848
Hasibullah Aman <hasib.aman7@gmail.com>
2022-12-08 09:58:09 +0430

    04

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementsByTagName("li")[2].style.color = "red";
 document.getElementsByClassName("btn").style.color = 'red';
 document.querySelector('#list a').style.color = 'red';
 document.getElementsByClassName("btn").style.backgroundColor = "yellow";
+
+function myfunction() {
+    document.querySelector("button")
+}
